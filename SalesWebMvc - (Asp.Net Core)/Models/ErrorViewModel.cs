ba473462d3b0ef3379370aed382eb32ea62dfc81
Erick H <henrique.eryck@gmail.com>
2020-08-23 12:50:46 -0300

    Project create

---
+++ SalesWebMvc - (Asp.Net Core)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMvc____Asp.Net_Core_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
